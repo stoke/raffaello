329641b50c7d36861e221eae660c678cf2eff2dc
stoke <stoke95@yahoo.it>
2012-10-24 13:10:34 +0200

    [feature] instance

--- test/test-application.js
+++ test/test-application.js
@@ -12,6 +12,10 @@ describe('Application', function() {
     expect(app.config).to.be.a('function');
   });
 
+  it('should have #load', function() {
+    expect(app.load).to.be.a('function');
+  });
+
   describe('#use', function() {
     var plugin = new raffaello.PluginFactory(), flag = false;
     plugin.setName('ubertest');
@@ -28,5 +32,47 @@ describe('Application', function() {
     it('should execute _constructor', function() {
       expect(flag).to.be.ok();
     });
+  });
+
+  describe('#load', function() {
+    var plugin = new raffaello.PluginFactory(), flag = false;
+    plugin.setName('ubertesta');
+    
+    plugin.defineMethod('test', function() {});
+    plugin.defineMethod('_constructor', function() { flag = true; })
+
+    app.load(plugin.plugin());
+
+    it('should load plugin', function() {  
+      expect(app.plugins._get('ubertesta')).to.be.an('object');
+    });
+
+    it('should not execute _constructor', function() {
+      expect(flag).not.to.be.ok();
+    });
+  });
+
+  describe('#Instance', function() {
+    var app = new raffaello.Application(),
+        plugin = new raffaello.PluginFactory(),
+        flag = false;
+    
+    plugin.setName('test');
+    plugin.extend({
+      _constructor: function() { flag = true; },
+      test: function() {}
+    });
+
+    app.load(plugin.plugin());
+    
+    var instance = app.Instance();
+
+    it('should return an instance which contains plugin methods', function() {
+      expect(instance.test).to.be.a('function');
+    });
+
+    it('should execute constructors of loaded plugins', function() {
+      expect(flag).to.be.ok();
+    })
   });
 });
